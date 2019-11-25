c2df0e414863b9621d4ca42c74a2b8d8d6501813
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-11-24 22:28:09 -0800

    add second api call for condition search, not working yet

--- src/doctor-api.js
+++ src/doctor-api.js
@@ -12,8 +12,17 @@ export class PracticeSearch {
     }
   }
 }
+export class ConditionSearch {
+  async getPracticeByCondition(userInputtedCondtion) {
+    try {
+      let response =  await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userInputtedCondition}&location=45.5051,-122.67,100&user_location=45.5051,-122.67&sort=distance-asc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      // alert("sorry, no doctors match your search criteria");
+      console.error("There was an error handling your request: " + error.message);
 
-// export class DoctorDetails{
-//   constructor()
-// } = function () {
-  // body...
+
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { PracticeSearch } from './../src/doctor-api.js';
+import { CondtionSearch } from './../src/doctor-api.js';
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
@@ -16,10 +17,29 @@ $(document).ready(function() {
     })();
 
 
-    // THIS WILL DISPLAY API FUNCTION RETURN ON (DOM)
-    function getElements(response) {
-      $("#showDoctorList").append(`<li>${response.data[0].name} ${response.data[0].phones[0].number} ${response.data[0].visit_address.street}  ${response.data[0].visit_address.city} ${response.data[0].visit_address.state} ${response.data[0].visit_address.zip}  <li>${response.data[1].name}<li>`);
+    $('#userConditionSearch').click(function(event) {
+      event.preventDefault();
+      const userInputtedCondtion = $('#ConditionInput').val();
+      (async () => {
+        let conditionSearch = new ConditionSearch();
+        const conditionResponse = await conditionSearch.getPracticeByCondition(userInputtedCondition);
+        getElements2(conditionResponse);
+        console.log("condition" conditionResponse);
+      })();
 
-    }
+
+      // THIS WILL DISPLAY API FUNCTION RETURN ON (DOM)
+      function getElements(response) {
+        $("#doctor1").text(`${response.data[0].name}  ${response.data[0].phones[0].number} ${response.data[0].visit_address.street}  ${response.data[0].visit_address.city} ${response.data[0].visit_address.state} ${response.data[0].visit_address.zip} ${response.data[0].accepts_new_patients}${response.data[0].website}`);
+        $("#doctor2").text(`${response.data[1].name} ${response.data[1].phones[0].number} ${response.data[1].visit_address.street}  ${response.data[1].visit_address.city} ${response.data[1].visit_address.state} ${response.data[1].visit_address.zip} ${response.data[1].accepts_new_patients}${response.data[1].website}`);
+        $("#doctor3").text(`${response.data[2].name} ${response.data[2].phones[0].number} ${response.data[2].visit_address.street}  ${response.data[2].visit_address.city} ${response.data[2].visit_address.state} ${response.data[2].visit_address.zip} ${response.data[2].accepts_new_patients}${response.data[2].website}`);
+        $("#doctor4").text(`${response.data[3].name} ${response.data[3].phones[0].number} ${response.data[3].visit_address.street}  ${response.data[3].visit_address.city} ${response.data[3].visit_address.state} ${response.data[3].visit_address.zip} ${response.data[3].accepts_new_patients}${response.data[3].website}`);
+
+      }
+
+      function getElements2(conditionResponse) {
+        $("#condition1").text(`${conditionResponse.data[0].name}  ${conditionResponse.data[0].phones[0].number} ${conditionResponse.data[0].visit_address.street}  ${conditionResponse.data[0].visit_address.city} ${conditionResponse.data[0].visit_address.state} ${conditionResponse.data[0].visit_address.zip} ${conditionResponse.data[0].accepts_new_patients}${conditionResponse.data[0].website}`);
+      }
+    });
   });
 });
